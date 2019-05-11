284d14397dd2a7dfbdffaaf35173282a07f82f63
Jonas <necrodon1@hotmail.com>
2019-05-11 13:01:01 +0200

    mangler sidste 3 sider

--- js/main.js
+++ js/main.js
@@ -1,35 +1,7 @@
 /*
 Menu navigation */
 
-var t1 = new TimelineMax({paused: true});
 
-t1.to(".one", 0.1, {    
-     y: 6,
-     rotation: 45,
-     ease: Expo.easeInOut
-});
-t1.to(".two", 0.1, {
-     y: -6,
-     rotation: -45,
-     ease: Expo.easeInOut,
-     delay: -0.1
-});
-
-t1.to(".three", 0, { display:'none'});
-
-
-t1.to(".menu", 1, {
-     top: "0%", 
-     ease: Expo.easeInOut,
-     delay: -0.5
-});
-
-t1.staggerFrom(".menu ul li", 0.8, {x: 200, opacity: 0, ease:Expo.easeOut}, 0.3);
-
-t1.reverse();
-$(document).on("click", ".toggle-btn", function() {
-     t1.reversed(!t1.reversed());
-});
 
 /* light-box galleri / forstørret billeder */
              $('a').nivoLightbox({
